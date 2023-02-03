76d1e6733c6b59e7a8f1b743c919c9ce86541004
Dmitry <118521147+annel1d@users.noreply.github.com>
2023-02-03 16:29:41 +0700

    4

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
 
 import pygame
+from pygame import font
 import requests
 
 first_coord = float(input("Первая координата (float):"))
 second_coord = float(input("Вторая координата (float):"))
 z = 10
+a = 'sat'
 map_file = "map.png"
 
 
 def fun():
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={first_coord},{second_coord}&z={z}&l=sat"
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={first_coord},{second_coord}&z={z}&l={a}"
     response = requests.get(map_request)
 
     if not response:
@@ -24,8 +26,92 @@ def fun():
         file.write(response.content)
     screen.blit(pygame.image.load(map_file), (0, 0))
 
+
 pygame.init()
+fps = 60
+fpsClock = pygame.time.Clock()
 screen = pygame.display.set_mode((600, 450))
+font = pygame.font.SysFont('Arial', 20)
+
+objects = []
+
+
+class Button():
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, eee=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.eee = eee
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+
+        self.alreadyPressed = False
+
+        objects.append(self)
+
+    def process(self):
+
+        mousePos = pygame.mouse.get_pos()
+
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+
+                if self.onePress:
+                    self.onclickFunction()
+                    self.eee()
+
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.eee()
+                    self.alreadyPressed = True
+
+            else:
+                self.alreadyPressed = False
+
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width / 2 - self.buttonSurf.get_rect().width / 2,
+            self.buttonRect.height / 2 - self.buttonSurf.get_rect().height / 2
+        ])
+        screen.blit(self.buttonSurface, self.buttonRect)
+
+
+def sput():
+    global a
+    print('Спутник!')
+    a = 'sat'
+
+
+def shema():
+    global a
+    print('Схема!')
+    a = 'map'
+
+
+def hybrid():
+    global a
+    print('Гибрид!')
+    a = 'sat,skl'
+
+
+customButton = Button(30, 30, 100, 100, '1. Схема', shema, fun)
+customButton = Button(30, 140, 100, 100, '2. Спутник', sput, fun)
+customButton = Button(30, 250, 100, 100, '3. Гибрид', hybrid, fun)
 fun()
 running = True
 
@@ -78,6 +164,9 @@ while running:
                     fun()
 
             ######################################
+
+    for object in objects:
+        object.process()
 
     pygame.display.flip()
 
