a60cf118899ee56271f1bceacd31b5ae3365037d
Dmitry <118521147+annel1d@users.noreply.github.com>
2023-02-03 15:44:27 +0700

    new task

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import requests
 
 first_coord = float(input("Первая координата (float):"))
 second_coord = float(input("Вторая координата (float):"))
-first_scale = 25.0
-second_scale = 25.0
+z = 10
 map_file = "map.png"
 
 
 def fun():
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={first_coord},{second_coord}&spn={first_scale},{second_scale}&l=sat"
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={first_coord},{second_coord}&z={z}&l=sat"
     response = requests.get(map_request)
 
     if not response:
@@ -38,18 +37,16 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_PAGEUP:
-                if first_scale >= 10.0 and first_scale >= 10.0:
-                    first_scale -= 5.0
-                    second_scale -= 5.0
-                    print(first_scale, second_scale)
+            if event.key == pygame.K_PAGEDOWN:
+                if z > 1:
+                    z -= 1
+                    print(z)
                     fun()
 
-            if event.key == pygame.K_PAGEDOWN:
-                if first_scale <= 90.0 and first_scale <= 90.0:
-                    first_scale += 5.0
-                    second_scale += 5.0
-                    print(first_scale, second_scale)
+            if event.key == pygame.K_PAGEUP:
+                if z < 13:
+                    z += 1
+                    print(z)
                     fun()
 
             ####################################
